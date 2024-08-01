7ba129b1222ebc5f8ca233f99d8778ca2172bb6c
Lachezar Kutsarov <lkutsarov@gmail.com>
2024-08-01 20:27:51 +0200

    Create Solution.ts

---
+++ Solution.ts
@@ -0,0 +1,16 @@
+
+function matrixSum(input: number[][]): number {
+    for (let row = 0; row < input.length; ++row) {
+        input[row].sort((x, y) => x - y);
+    }
+
+    let matrixSum = 0;
+    for (let column = input[0].length - 1; column >= 0; --column) {
+        let maxInColumn = 0;
+        for (let row = 0; row < input.length; ++row) {
+            maxInColumn = Math.max(input[row][column], maxInColumn);
+        }
+        matrixSum += maxInColumn;
+    }
+    return matrixSum;
+};
